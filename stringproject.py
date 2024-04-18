d92beee84da4543f7e3e2d8ead1cccf419d1316d
cristianocastle <99284602+cristianocastle@users.noreply.github.com>
2024-04-18 14:20:48 -0700

    update

--- stringproject.py
+++ stringproject.py
@@ -2,15 +2,68 @@
 
 name = input('Enter Name: ')
 
+
+#Display the number of characters in the string
 def num_char():
     pass
 
+
+#Display the last character in the string
 def last_char(): 
     pass 
 
+
+#Display the location of the first occurrence of the letter ‘e’ (0 if not found)
 def e_char(): 
     pass
 
+
+#Display the number of words in the string
 def num_words(): 
     pass
+
+
+Display the first word of the string
+def first_word():
+    pass
+
+
+#Display the number of vowels in the string
+def num_vowels(): 
+    pass
+
+#Display the string after capitalizing all vowels in the string. 
+def cap_vowels(): 
+    pass
+
+
+#Display the string centered between 50 ‘~’s, and 70 ‘+’s
+def center_string():
+    pass
+
+
+#Display the string split in half on either end of 70 ‘*’s.
+def split_string():
+    pass
+
+'''
+
+--- PART 2 ----
+
+'''
+
+favstring = '''Triple Quoted String'''
+
+#generates a string and containing original string and the string backwards
+def mirror_string(): 
+    pass
+
+#removes all occurences of a given letter 
+def remove_char():
+    pass
+
+
+#counts number of alphabetic characters 
+def num_alphabetic(): 
+
 
